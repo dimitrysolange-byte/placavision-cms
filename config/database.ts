b86ba1ff020ab7a3f6fad4a68e4dd5ef106b850d
dimitrysolange-byte <dimitrysolange@gmail.com>
2025-12-23 11:46:54 -0600

    Fix database config for production (Render)

---
+++ config/database.ts
@@ -0,0 +1,15 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+});
